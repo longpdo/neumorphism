16137aafda82e37a90164a7aa0c28e4efdd1e698
Long Do <ldo@adorsys.de>
2020-05-28 14:14:47 +0200

    Fix preloader not closing bug in Chrome, Safari

--- _js/app.js
+++ _js/app.js
@@ -1,46 +1,41 @@
-window.onbeforeunload = function() {
-  window.scrollTo(0, 0);
-};
+setTimeout(function() {
+  fadeOutPreloader(document.getElementById('preloader'), 69);
+}, 1500);
 
-document.addEventListener(
-  'DOMContentLoaded',
-  function() {
-    // Fade out preloader
-    setTimeout(function() {
-      fadeOutPreloader(document.getElementById('preloader'), 69);
-    }, 1500);
+$(document).ready(function() {
+  $(window).on('beforeunload', function() {
+    window.scrollTo(0, 0);
+  });
 
-    /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-    particlesJS.load('landing', 'assets/particles.json', function() {});
+  /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+  particlesJS.load('landing', 'assets/particles.json', function() {});
 
-    // Typing Text
-    var element = document.getElementById('txt-rotate');
-    var toRotate = element.getAttribute('data-rotate');
-    var period = element.getAttribute('data-period');
-    setTimeout(function() {
-      new TxtRotate(element, JSON.parse(toRotate), period);
-    }, 1500);
+  // Typing Text
+  var element = document.getElementById('txt-rotate');
+  var toRotate = element.getAttribute('data-rotate');
+  var period = element.getAttribute('data-period');
+  setTimeout(function() {
+    new TxtRotate(element, JSON.parse(toRotate), period);
+  }, 1500);
 
-    // INJECT CSS
-    var css = document.createElement('style');
-    css.type = 'text/css';
-    css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
-    document.body.appendChild(css);
+  // INJECT CSS
+  var css = document.createElement('style');
+  css.type = 'text/css';
+  css.innerHTML = '#txt-rotate > .wrap { border-right: 0.08em solid #666 }';
+  document.body.appendChild(css);
 
-    // Initialize AOS
-    AOS.init({
-      disable: 'mobile',
-      offset: 200,
-      duration: 600,
-      easing: 'ease-in-sine',
-      delay: 100,
-      once: true
-    });
+  // Initialize AOS
+  AOS.init({
+    disable: 'mobile',
+    offset: 200,
+    duration: 600,
+    easing: 'ease-in-sine',
+    delay: 100,
+    once: true
+  });
 
-    randomizeOrder();
-  },
-  false
-);
+  randomizeOrder();
+});
 
 /* FUNCTIONS */
 /* Preloader */
